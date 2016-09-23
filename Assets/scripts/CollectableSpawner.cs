b4c791f0582725cca9272b054a6bb3e388cd6a28
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 23:13:59 +0100

    Add CollectableSpawner

---
+++ Assets/scripts/CollectableSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject collectable, badCollectable;
+	[SerializeField]
+	private float collectableSpawnTime, badCollectableSpawnTime;
+	[SerializeField]
+	private bool canSpawnCollectable, canSpawnBadCollectable;
+	private float xClamp, yClamp;
+
+	private void Start () {
+		xClamp = Camera.main.orthographicSize * Screen.width / Screen.height - 1;
+		yClamp = Camera.main.orthographicSize - 1;
+		StartCoroutine (SpawnCollectables ());
+		StartCoroutine (SpawnBadCollectables ());
+	}
+
+	private IEnumerator SpawnCollectables () {
+		while (canSpawnCollectable) {
+			yield return new WaitForSeconds (collectableSpawnTime);
+			SpawnCollectable ();
+		}
+	}
+
+	private IEnumerator SpawnBadCollectables () {
+		while (canSpawnBadCollectable) {
+			yield return new WaitForSeconds (badCollectableSpawnTime);
+			SpawnBadCollectable ();
+		}
+	}
+
+	private void SpawnCollectable () {
+		Instantiate (
+			collectable,
+			new Vector3(Random.Range(-xClamp, xClamp), Random.Range(-yClamp, yClamp), 0),
+			Quaternion.identity
+		);
+	}
+
+	private void SpawnBadCollectable () {
+		Instantiate (
+			badCollectable,
+			new Vector3(Random.Range(-xClamp, xClamp), Random.Range(-yClamp, yClamp), 0),
+			Quaternion.identity
+		);
+	}
+}
