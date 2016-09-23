0b2e1f4617d707e6fbb9e0979a0ba495b4bdc8f6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 23:00:26 +0100

    Move player to mouse position and disable cursor

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed = 1f;
+
+	private void Start () {
+		HideMouseCursor ();
+	}
+
+	private void Update () {
+		MovePlayerToMouse ();
+	}
+
+	private void MovePlayerToMouse () {
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = transform.position.z - Camera.main.transform.position.z;
+		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
+		transform.position = Vector3.MoveTowards (transform.position, mousePos, moveSpeed * Time.deltaTime);
+	}
+
+	private void HideMouseCursor () {
+		Cursor.visible = false;
+	}
+}
